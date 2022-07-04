2b92587f499c8ebb1c57cccc32e6ce6e54788285
goku-g <gokarnabaskota500@gmail.com>
2022-07-05 00:41:36 +0545

    recieve data from esp8266 and insert into database.

---
+++ postUpload.php
@@ -0,0 +1,37 @@
+<?php
+    include('config.php');
+
+    $var = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $data=test_input($_POST['distance']);
+        $remark=test_input($_POST['remarks']);
+
+        if($data && $remark)
+        {
+            
+            $con->query("INSERT INTO distance_data (id, distance, timestamp, remarks) VALUES (NULL, '".$data."', current_timestamp(), '".$remark."');");
+            header("location: index.php");
+            $var = "ok";
+
+        }
+        else
+        {
+            $var = "invalid";
+        }
+        
+        
+    }
+    else
+    {
+        $var = "There is no POST requests.";
+    }
+    echo $var;
+
+    function test_input($value) {
+        $value = trim($value);
+        $value = stripslashes($value);
+        $value = htmlspecialchars($value);
+        return $value;
+    }
+?>
